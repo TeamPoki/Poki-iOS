110db86fb9eb80c8dc8b93497e1bb1abf3b643c9
Hongsik Choi <playhong16@gmail.com>
2023-10-16 16:04:24 +0900

    [FEAT]: #2 mainImageView 구현

--- Poki-iOS/Poki-iOS/Controllers/DetailPhotoViewController.swift
+++ Poki-iOS/Poki-iOS/Controllers/DetailPhotoViewController.swift
@@ -21,13 +21,20 @@ final class DetailPhotoViewController: UIViewController {
     }()
     
     private let dateLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont(name: Constants.fontBold, size: 16)
         label.text = "2023. 10. 16"
         label.textColor = .white
         label.textAlignment = .center
         return label
+    }()
+    
+    private let mainImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.image = UIImage(named: "인생네컷")
+        return iv
     }()
 
     // MARK: - Life Cycle
@@ -40,7 +47,7 @@ final class DetailPhotoViewController: UIViewController {
     
     // MARK: - Helper
     private func addSubViews() {
-        view.addSubviews(titleLabel, dateLabel)
+        view.addSubviews(titleLabel, dateLabel, mainImageView)
     }
     
     private func setupLayout() {
@@ -53,6 +60,12 @@ final class DetailPhotoViewController: UIViewController {
             dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
             dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        NSLayoutConstraint.activate([
+            mainImageView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 20),
+            mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mainImageView.heightAnchor.constraint(equalToConstant: 537)
         ])
     }
 }
