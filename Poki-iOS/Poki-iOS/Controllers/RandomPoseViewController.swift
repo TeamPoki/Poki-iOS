6ea32a0c435807350a33aa8b789b31fe3fc9c9ed
Hongsik Choi <playhong16@gmail.com>
2023-10-17 15:07:43 +0900

    [FEAT]: #12 랜덤 포즈 페이지 - bookmarkButton 구현

--- Poki-iOS/Poki-iOS/Controllers/RandomPoseViewController.swift
+++ Poki-iOS/Poki-iOS/Controllers/RandomPoseViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class RandomPoseViewController: UIViewController {
     
+    // MARK: - Components
+    
     private let poseImageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
@@ -18,25 +20,45 @@ final class RandomPoseViewController: UIViewController {
     
     private let refreshButton: UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .black
         button.titleLabel?.font = UIFont(name: Constants.fontSemiBold, size: 16)
-        button.titleLabel?.tintColor = .white
         button.setTitle("다른 포즈보기", for: .normal)
         button.addTarget(self, action: #selector(refreshButtonTapped), for: .touchUpInside)
         button.layer.cornerRadius = 8
         return button
     }()
+    
+    private let bookmarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "star"), for: .normal)
+        button.layer.cornerRadius = 30
+        return button
+    }()
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configure()
         addSubviews()
         setupLayout()
     }
     
+    // MARK: - Helpers
+    
+    private func configure() {
+        configure(refreshButton)
+        configure(bookmarkButton)
+    }
+    
+    private func configure(_ button: UIButton) {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .black
+        button.tintColor = .white
+    }
+    
     private func addSubviews() {
-        view.addSubviews(poseImageView, refreshButton)
+        view.addSubviews(poseImageView, bookmarkButton, refreshButton)
     }
     
     private func setupLayout() {
@@ -47,7 +69,13 @@ final class RandomPoseViewController: UIViewController {
             poseImageView.heightAnchor.constraint(equalToConstant: 452)
         ])
         NSLayoutConstraint.activate([
-            refreshButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bookmarkButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bookmarkButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            bookmarkButton.heightAnchor.constraint(equalToConstant: 60),
+            bookmarkButton.widthAnchor.constraint(equalToConstant: 60)
+        ])
+        NSLayoutConstraint.activate([
+            refreshButton.leadingAnchor.constraint(equalTo: bookmarkButton.trailingAnchor, constant: 20),
             refreshButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             refreshButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             refreshButton.heightAnchor.constraint(equalToConstant: 60)
@@ -55,6 +83,7 @@ final class RandomPoseViewController: UIViewController {
     }
     
     // MARK: - Actions
+    
     @objc private func refreshButtonTapped(_ sender: UIButton) {
         print("새로고침 버튼 눌렀음요~")
     }
