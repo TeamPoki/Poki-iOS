354bedf99d62b81732e449beddd2ab94d3fc0c1b
Hongsik Choi <playhong16@gmail.com>
2023-10-17 13:34:54 +0900

    [FEAT]: #12 RandomPoseViewController 생성

---
+++ Poki-iOS/Poki-iOS/Controllers/RandomPoseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RandomPoseViewController.swift
+//  Poki-iOS
+//
+//  Created by playhong on 2023/10/17.
+//
+
+import UIKit
+
+class RandomPoseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
