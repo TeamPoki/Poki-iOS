c07171796c53179ecd6c7873f382ca0ee0378111
Hongsik Choi <playhong16@gmail.com>
2023-10-17 14:20:59 +0900

    [FEAT]: #12 랜덤 포즈 페이지 - refreshButton 구현

--- Poki-iOS/Poki-iOS/Controllers/RandomPoseViewController.swift
+++ Poki-iOS/Poki-iOS/Controllers/RandomPoseViewController.swift
@@ -15,6 +15,18 @@ final class RandomPoseViewController: UIViewController {
         iv.image = UIImage(named: "alone-pose-1")
         return iv
     }()
+    
+    private let refreshButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .black
+        button.titleLabel?.font = UIFont(name: Constants.fontSemiBold, size: 16)
+        button.titleLabel?.tintColor = .white
+        button.setTitle("다른 포즈보기", for: .normal)
+        button.addTarget(self, action: #selector(refreshButtonTapped), for: .touchUpInside)
+        button.layer.cornerRadius = 8
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +36,7 @@ final class RandomPoseViewController: UIViewController {
     }
     
     private func addSubviews() {
-        view.addSubviews(poseImageView)
+        view.addSubviews(poseImageView, refreshButton)
     }
     
     private func setupLayout() {
@@ -34,5 +46,16 @@ final class RandomPoseViewController: UIViewController {
             poseImageView.widthAnchor.constraint(equalToConstant: 150),
             poseImageView.heightAnchor.constraint(equalToConstant: 452)
         ])
+        NSLayoutConstraint.activate([
+            refreshButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            refreshButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            refreshButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            refreshButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    // MARK: - Actions
+    @objc private func refreshButtonTapped(_ sender: UIButton) {
+        print("새로고침 버튼 눌렀음요~")
     }
 }
