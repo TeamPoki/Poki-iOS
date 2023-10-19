606feccefabd9fe7c734b24f0cf17bdfe942af81
Taeshiki <durudurutta@gmail.com>
2023-10-19 16:46:35 +0900

    [Feat]: 설정 페이지 UI구현

--- Poki-iOS/Poki-iOS/Controllers/SettingVC/SettingsViewController.swift
+++ Poki-iOS/Poki-iOS/Controllers/SettingVC/SettingsViewController.swift
@@ -12,16 +12,16 @@ import Then
 class SettingsViewController: UIViewController {
     
     // MARK: - Properties
-    
-    
-    
+    private var tableView: UITableView!
+    private var data: [String] = ["공지사항", "개인정보 처리방침", "서비스 이용약관", "탈퇴하기"]
     
     // MARK: - Life Cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         configureNav()
+        setupTableView()
         
     }
     
@@ -44,15 +44,69 @@ class SettingsViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
+    private func setupTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.trailing.bottom.equalToSuperview()
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+            tableView.separatorStyle = .singleLine
+        }
+    }
     
     
     
     
-
     // MARK: - Actions
     
     
     
     
-
+    
+}
+// MARK: - UITableViewDataSource 및 UITableViewDelegate 구현
+extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let image = UIImageView(image: UIImage(systemName: "chevron.right"))
+        image.tintColor = .black
+        cell.contentView.addSubview(image)
+        image.snp.makeConstraints { make in
+            make.trailing.equalTo(cell.contentView).offset(-16)
+            make.centerY.equalTo(cell.contentView)
+        }
+        
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.font = UIFont(name: "font", size: 12)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedData = data[indexPath.row]
+        
+        //print("선택한 항목: \(selectedData)")
+        if selectedData == "공지사항" {
+//            let noticeListViewController = NoticeListViewController()
+//            navigationController?.pushViewController(noticeListViewController, animated: true)
+            print("공지사항 눌림")
+        } else if selectedData == "탈퇴하기" {
+            let accountDeletionViewController = AccountDeletionViewController()
+            navigationController?.pushViewController(accountDeletionViewController, animated: true)
+        }
+    }
 }
