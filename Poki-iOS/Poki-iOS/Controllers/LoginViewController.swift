4355a05a5d9441a577fa7eb3962d65d276011fdb
Hongsik Choi <playhong16@gmail.com>
2023-10-23 11:58:31 +0900

    #87 [Remove]: EmailLoginViewController.swift 파일 삭제

--- Poki-iOS/Poki-iOS/Controllers/LoginViewController.swift
+++ Poki-iOS/Poki-iOS/Controllers/LoginViewController.swift
@@ -12,29 +12,7 @@ import Then
 class LoginViewController: UIViewController {
     
     // MARK: - Components
-
-    private let kakakoLoginButton = UIButton().then {
-        $0.setImage(UIImage(named: "Kakao Login"), for: .normal)
-    }
     
-    private let emailLoginButton = UIButton().then {
-        $0.setImage(UIImage(named: "Email Login"), for: .normal)
-        $0.addTarget(self, action: #selector(emailLoginButtonTapped), for: .touchUpInside)
-    }
-    
-    private let signUpButton = UIButton().then {
-        $0.titleLabel?.font = UIFont(name: Constants.fontMedium, size: 12)
-        $0.setTitle("회원가입", for: .normal)
-        $0.setTitleColor(UIColor.gray, for: .normal)
-        $0.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
-    }
-
-    private let buttonStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.alignment = .fill
-        $0.distribution = .fill
-        $0.spacing = 15
-    }
     
     // MARK: - Life Cycle
     
@@ -48,24 +26,12 @@ class LoginViewController: UIViewController {
     // MARK: - Helpers
     
     private func addSubviews() {
-        buttonStackView.addArrangedSubviews(kakakoLoginButton, emailLoginButton, signUpButton)
-        view.addSubviews(buttonStackView)
     }
     
     private func setupLayout() {
-        buttonStackView.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(20)
-            $0.trailing.equalToSuperview().inset(20)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
-        }
     }
     
     // MARK: - Actions
-    @objc private func emailLoginButtonTapped(_ sender: UIButton) {
-        let moveVC = EmailLoginViewController()
-        navigationController?.pushViewController(moveVC, animated: true)
-    }
-    
     @objc private func signUpButtonTapped(_ sender: UIButton) {
         print("회원가입 버튼 눌렀어유~")
     }
