7a6d672c4a1d6dda07e6c13904866eea56e44d43
Hongsik Choi <playhong16@gmail.com>
2023-10-24 14:29:58 +0900

    #98 [Add]: 포즈 카테고리 파일 생성

---
+++ Poki-iOS/Poki-iOS/Models/PoseCategory.swift
@@ -0,0 +1,14 @@
+//
+//  PoseCategory.swift
+//  Poki-iOS
+//
+//  Created by playhong on 2023/10/24.
+//
+
+import UIKit
+
+enum PoseCategory {
+    case alone
+    case twoPeople
+    case manyPeople
+}
