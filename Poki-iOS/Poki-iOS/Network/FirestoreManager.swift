7edb34b2d2a69013c963dc7de2cab61ad7383f5a
Hongsik Choi <playhong16@gmail.com>
2023-10-25 17:38:47 +0900

    #109 [Add]: FirestoreManager 파일 추가

---
+++ Poki-iOS/Poki-iOS/Network/FirestoreManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirestoreManager.swift
+//  Poki-iOS
+//
+//  Created by playhong on 2023/10/25.
+//
+
+import UIKit
+import FirebaseFirestore
+import FirebaseStorage
+import FirebaseFirestoreSwift
+
+class FirestoreManager {
+    
+}
