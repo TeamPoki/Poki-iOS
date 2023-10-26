fd0e6f589156d5c99c9207d21fc8b4f469d506ef
Taeshiki <durudurutta@gmail.com>
2023-10-26 16:59:38 +0900

    [Feat]: LikedPoseImageDetailViewController 파일 추가

---
+++ Poki-iOS/Poki-iOS/Controllers/MyPageVC/LikedPoseImageDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LikedPoseImageDetailViewController.swift
+//  Poki-iOS
+//
+//  Created by 요시킴 on 2023/10/26.
+//
+
+import UIKit
+
+class LikedPoseImageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
