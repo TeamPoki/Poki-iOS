3734f7cb2c21ea0ba68bbcfd4733b0475caf90dd
Hongsik Choi <playhong16@gmail.com>
2023-11-01 01:39:45 +0900

    #191 [Refactor]: 회원가입 페이지 뷰 분리

--- Poki-iOS/Poki-iOS/Sources/Presenter/Login/SignUp/SignUpVC.swift
+++ Poki-iOS/Poki-iOS/Sources/Presenter/Login/SignUp/SignUpVC.swift
@@ -50,111 +50,15 @@ final class SignUpVC: UIViewController {
         return frame
     }
 
-    // MARK: - Components
-    private let emailPlaceholder = UILabel().then {
-        $0.text = "이메일"
-        $0.font = UIFont(name: Constants.fontRegular, size: 16)
-        $0.textColor = .lightGray
-    }
-    
-    private let emailTextField = UITextField().then {
-        $0.keyboardType = .emailAddress
-    }
-    
-    private let emailTextFieldView = UIView().then {
-        $0.backgroundColor = .clear
-    }
-    private let emailHintLabel = UILabel().then {
-        $0.text = "이메일 형식으로 입력해주세요."
-        $0.font = UIFont(name: Constants.fontRegular, size: 12)
-        $0.textColor = .black
-    }
-    
-    private let passwordPlaceholder = UILabel().then {
-        $0.text = "비밀번호"
-        $0.font = UIFont(name: Constants.fontRegular, size: 16)
-        $0.textColor = .lightGray
-    }
-    
-    private let passwordTextField = UITextField().then {
-        $0.isSecureTextEntry = true
-    }
-    
-    private let passwordTextFieldView = UIView().then {
-        $0.backgroundColor = .clear
-    }
-    
-    private lazy var eyeButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "eye"), for: .normal)
-        $0.setImage(UIImage(systemName: "eye.slash"), for: .selected)
-        $0.tintColor = .lightGray
-        $0.addTarget(self, action: #selector(eyeButtonTapped), for: .touchUpInside)
-    }
-    
-    private let passwordHintLabel = UILabel().then {
-        $0.text = "8~20자, 영문, 숫자, 특수문자를 포함해주세요."
-        $0.font = UIFont(name: Constants.fontRegular, size: 12)
-        $0.textColor = .black
-    }
-    
-    private let nicknamePlaceholder = UILabel().then {
-        $0.text = "닉네임"
-        $0.font = UIFont(name: Constants.fontRegular, size: 16)
-        $0.textColor = .lightGray
-    }
-    
-    private let nicknameTextField = UITextField().then {
-        $0.keyboardType = .default
-    }
-    
-    private let nicknameTextFieldView = UIView().then {
-        $0.backgroundColor = .clear
-    }
-    
-    private let nicknameHintLabel = UILabel().then {
-        $0.text = "2~8자, 영문, 한글만 입력할 수 있습니다."
-        $0.font = UIFont(name: Constants.fontRegular, size: 12)
-        $0.textColor = .black
-    }
-    
-    private lazy var signUpButton = UIButton().then {
-        $0.setTitle("가입하기", for: .normal)
-        $0.setTitleColor(.white, for: .normal)
-        $0.titleLabel?.font = UIFont(name: Constants.fontBold, size: 16)
-        $0.layer.cornerRadius = 25
-        $0.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
-    }
-    
-    private lazy var agreeToTermsOfServiceButton = UIButton().then {
-        $0.contentHorizontalAlignment = .left
-        $0.tintColor = .lightGray
-        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25)
-        $0.setImage(UIImage(systemName: "square", withConfiguration: imageConfig), for: .normal)
-        $0.setImage(UIImage(systemName: "checkmark.square.fill", withConfiguration: imageConfig), for: .selected)
-        $0.addTarget(self, action: #selector(agreeToTermsOfServiceButtonTapped), for: .touchUpInside)
-        $0.clipsToBounds = true
-    }
-    
-    private let agreeToTermsOfServiceLabel = UILabel().then {
-        $0.font = UIFont(name: Constants.fontMedium, size: 14)
-        $0.text = "서비스 이용약관에 동의합니다."
-        $0.textColor = .lightGray
-    }
-    
-    private let agreeToTermsOfServiceStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.distribution = .fill
-        $0.alignment = .fill
-        $0.spacing = 3
-    }
-    
     // MARK: - Life Cycle
+    let signUpView = SignUpView()
+    override func loadView() {
+        self.view = signUpView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        addSubviews()
-        setupLayout()
         updateSignUpButton()
     }
     
@@ -165,9 +69,6 @@ final class SignUpVC: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        drawUnderline(emailTextFieldView)
-        drawUnderline(passwordTextFieldView)
-        drawUnderline(nicknameTextFieldView)
     }
     
     // MARK: - Helpers
@@ -175,102 +76,11 @@ final class SignUpVC: UIViewController {
     private func configureUI() {
         view.backgroundColor = .white
         configureNav()
-        configure(emailTextField)
-        configure(passwordTextField)
-        configure(nicknameTextField)
     }
     
     private func configureNav() {
         navigationItem.title = "회원가입"
         navigationController?.configureBasicAppearance()
-    }
-    
-    private func configure(_ textField: UITextField) {
-        textField.backgroundColor = .clear
-        textField.textColor = .black
-        textField.tintColor = .black
-        textField.autocapitalizationType = .none
-        textField.autocorrectionType = .no
-        textField.spellCheckingType = .no
-        textField.delegate = self
-        textField.layer.masksToBounds = true
-        textField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
-    }
-    
-    private func addSubviews() {
-        emailTextFieldView.addSubviews(emailTextField, emailPlaceholder)
-        passwordTextFieldView.addSubviews(passwordTextField, passwordPlaceholder)
-        nicknameTextFieldView.addSubviews(nicknameTextField, nicknamePlaceholder)
-        agreeToTermsOfServiceStackView.addArrangedSubviews(agreeToTermsOfServiceButton, agreeToTermsOfServiceLabel)
-        passwordTextFieldView.addSubview(eyeButton)
-        view.addSubviews(emailTextFieldView, emailHintLabel, passwordTextFieldView, passwordHintLabel, nicknameTextFieldView, nicknameHintLabel, agreeToTermsOfServiceStackView, signUpButton)
-    }
-    
-    private func setupLayout() {
-        emailTextFieldView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
-            $0.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(50)
-        }
-        emailTextField.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(8)
-        }
-        emailPlaceholder.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalTo(emailTextField)
-        }
-        emailHintLabel.snp.makeConstraints {
-            $0.top.equalTo(emailTextFieldView.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(30)
-        }
-        passwordTextFieldView.snp.makeConstraints {
-            $0.top.equalTo(emailTextFieldView.snp.bottom).offset(40)
-            $0.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(50)
-        }
-        passwordTextField.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalToSuperview()
-        }
-        passwordHintLabel.snp.makeConstraints {
-            $0.top.equalTo(passwordTextFieldView.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(30)
-        }
-        eyeButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(10)
-            $0.centerY.equalTo(passwordTextField)
-        }
-        passwordPlaceholder.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalTo(passwordTextField)
-        }
-        nicknameTextFieldView.snp.makeConstraints {
-            $0.top.equalTo(passwordTextFieldView.snp.bottom).offset(40)
-            $0.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(50)
-        }
-        nicknameTextField.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(8)
-        }
-        nicknamePlaceholder.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(8)
-            $0.centerY.equalTo(nicknameTextField)
-        }
-        nicknameHintLabel.snp.makeConstraints {
-            $0.top.equalTo(nicknameTextFieldView.snp.bottom)
-            $0.leading.trailing.equalToSuperview().inset(30)
-        }
-        agreeToTermsOfServiceStackView.snp.makeConstraints {
-            $0.top.equalTo(nicknameTextFieldView.snp.bottom).offset(40)
-            $0.leading.trailing.equalToSuperview().inset(20)
-        }
-        signUpButton.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(20)
-            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-10)
-            $0.height.equalTo(50)
-        }
     }
     
     private func drawUnderline(_ view: UIView) {
@@ -298,12 +108,12 @@ final class SignUpVC: UIViewController {
 
     private func updateSignUpButton() {
         if isSignUpFormValid == true {
-            signUpButton.isEnabled = true
+            self.signUpView.signUpButton.isEnabled = true
         }
         if isSignUpFormValid == false {
-            signUpButton.isEnabled = false
+            self.signUpView.signUpButton.isEnabled = false
         }
-        signUpButton.backgroundColor = self.signUpButtonColor
+        self.signUpView.signUpButton.backgroundColor = self.signUpButtonColor
     }
     
     // MARK: - Validation
@@ -328,13 +138,13 @@ final class SignUpVC: UIViewController {
     // MARK: - Actions
     
     @objc private func textDidChange(_ sender: UITextField) {
-        if sender == emailTextField {
+        if sender == self.signUpView.emailTextField {
             self.email = sender.text
         }
-        if sender == passwordTextField {
+        if sender == self.signUpView.passwordTextField {
             self.password = sender.text
         }
-        if sender == nicknameTextField {
+        if sender == self.signUpView.nicknameTextField {
             self.nickname = sender.text
         }
         self.updateSignUpButton()
@@ -343,10 +153,10 @@ final class SignUpVC: UIViewController {
     @objc private func eyeButtonTapped(_ sender: UIButton) {
         sender.isSelected.toggle()
         if sender.isSelected == true {
-            passwordTextField.isSecureTextEntry = false
+            self.signUpView.passwordTextField.isSecureTextEntry = false
         }
         if sender.isSelected == false {
-            passwordTextField.isSecureTextEntry = true
+            self.signUpView.passwordTextField.isSecureTextEntry = true
         }
     }
     
@@ -354,12 +164,12 @@ final class SignUpVC: UIViewController {
         sender.isSelected.toggle()
         if sender.isSelected == true {
             sender.tintColor = .black
-            agreeToTermsOfServiceLabel.textColor = .black
+            self.signUpView.agreeToTermsOfServiceLabel.textColor = .black
             self.isAgree = true
         }
         if sender.isSelected == false {
             sender.tintColor = .lightGray
-            agreeToTermsOfServiceLabel.textColor = .lightGray
+            self.signUpView.agreeToTermsOfServiceLabel.textColor = .lightGray
             self.isAgree = false
         }
         self.updateSignUpButton()
@@ -395,14 +205,14 @@ final class SignUpVC: UIViewController {
 // MARK: - UITextFieldDelegate
 extension SignUpVC: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == emailTextField {
-            updateLayout(emailPlaceholder, textField: textField)
+        if textField == self.signUpView.emailTextField {
+            updateLayout(self.signUpView.emailPlaceholder, textField: textField)
         }
-        if textField == passwordTextField {
-            updateLayout(passwordPlaceholder, textField: textField)
+        if textField == self.signUpView.passwordTextField {
+            updateLayout(self.signUpView.passwordPlaceholder, textField: textField)
         }
-        if textField == nicknameTextField {
-            updateLayout(nicknamePlaceholder, textField: textField)
+        if textField == self.signUpView.nicknameTextField {
+            updateLayout(self.signUpView.nicknamePlaceholder, textField: textField)
         }
     }
     
@@ -417,14 +227,14 @@ extension SignUpVC: UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == emailTextField, textField.text == "" {
-            resetLayout(emailPlaceholder, textField: textField)
+        if textField == self.signUpView.emailTextField, textField.text == "" {
+            resetLayout(self.signUpView.emailPlaceholder, textField: textField)
         }
-        if textField == passwordTextField, textField.text == "" {
-            resetLayout(passwordPlaceholder, textField: textField)
+        if textField == self.signUpView.passwordTextField, textField.text == "" {
+            resetLayout(self.signUpView.passwordPlaceholder, textField: textField)
         }
-        if textField == nicknameTextField, textField.text == "" {
-            resetLayout(nicknamePlaceholder, textField: textField)
+        if textField == self.signUpView.nicknameTextField, textField.text == "" {
+            resetLayout(self.signUpView.nicknamePlaceholder, textField: textField)
         }
     }
     
@@ -439,14 +249,14 @@ extension SignUpVC: UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == emailTextField {
-            passwordTextField.becomeFirstResponder()
+        if textField == self.signUpView.emailTextField {
+            self.signUpView.passwordTextField.becomeFirstResponder()
         }
-        if textField == passwordTextField {
-            nicknameTextField.becomeFirstResponder()
+        if textField == self.signUpView.passwordTextField {
+            self.signUpView.nicknameTextField.becomeFirstResponder()
         }
-        if textField == nicknameTextField {
-            nicknameTextField.resignFirstResponder()
+        if textField == self.signUpView.nicknameTextField {
+            self.signUpView.nicknameTextField.resignFirstResponder()
         }
         return false
     }
