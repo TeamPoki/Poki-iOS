574cd1c66de3abf5ce17dce006e2bff462196a26
Hongsik Choi <playhong16@gmail.com>
2023-11-02 23:44:51 +0900

    #239 [Refactor]: StorageManager - 스토리지에 이미지 업로드 메서드 수정

--- Poki-iOS/Poki-iOS/Sources/Manager/StorageManager.swift
+++ Poki-iOS/Poki-iOS/Sources/Manager/StorageManager.swift
@@ -64,6 +64,56 @@ final class StorageManager {
         }
     }
     
+    func uploadPhotoImage(image: [UIImage], completion: @escaping (Result<(URL, URL), Error>) -> Void) {
+        guard image.count >= 2 else {
+            completion(.failure(FirebaseError.imageCountError))
+            return
+        }
+        
+        let photoImageData = image[0].jpegData(compressionQuality: 0.4)
+        let tagImageData = image[1].jpegData(compressionQuality: 0.4)
+
+        guard let photoData = photoImageData, let tagData = tagImageData else {
+            completion(.failure(FirebaseError.imageDataError))
+            return
+        }
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        let imageName = UUID().uuidString + String(Date().timeIntervalSince1970)
+        
+        let firebaseReference = Storage.storage().reference().child(imageName)
+
+        // Upload the first image.
+        firebaseReference.child("photo.jpg").putData(photoData, metadata: metaData) { _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            // Upload the second image.
+            firebaseReference.child("tag.jpg").putData(tagData, metadata: metaData) { _, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                // Get download URLs for both images.
+                firebaseReference.child("photo.jpg").downloadURL { photoURL, _ in
+                    firebaseReference.child("tag.jpg").downloadURL { tagURL, _ in
+                        if let photoURL = photoURL, let tagURL = tagURL {
+                            // Return the download URLs in the completion handler.
+                            completion(.success((photoURL, tagURL)))
+                        } else {
+                            completion(.failure(FirebaseError.downloadURLError))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     func downloadImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
         let storageReference = Storage.storage().reference(forURL: urlString)
         let megaByte = Int64(4.9 * 1024 * 1024)
