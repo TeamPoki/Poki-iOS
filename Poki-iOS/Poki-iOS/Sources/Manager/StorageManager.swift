197c7f0734707cf1766046075b055d6855266b2c
Hongsik Choi <playhong16@gmail.com>
2023-11-06 02:30:08 +0900

    #262 [ETC]: 사용하지 않는 메서드 주석 처리

--- Poki-iOS/Poki-iOS/Sources/Manager/StorageManager.swift
+++ Poki-iOS/Poki-iOS/Sources/Manager/StorageManager.swift
@@ -14,56 +14,56 @@ final class StorageManager {
     private init() {}
     
     // 아직 다른곳에서 사용하고 있어서 삭제 안했습니다. 추후 메서드 리팩토링 시 삭제 예정
-    func photoUploadImage(image: [UIImage], date: String, memo: String, tagText: String, completion: @escaping (Result<(URL, URL), Error>) -> Void) {
-        guard let userUID = authManager.currentUserUID else { return }
-        guard image.count >= 2 else {
-            completion(.failure(FirebaseError.imageCountError))
-            return
-        }
-        
-        let photoImageData = image[0].jpegData(compressionQuality: 0.4)
-        let tagImageData = image[1].jpegData(compressionQuality: 0.4)
-
-        guard let photoData = photoImageData, let tagData = tagImageData else {
-            completion(.failure(FirebaseError.imageDataError))
-            return
-        }
-        
-        let metaData = StorageMetadata()
-        metaData.contentType = "image/jpeg"
-        
-        let imageName = userUID
-        
-        let firebaseReference = Storage.storage().reference().child(imageName).child(UUID().uuidString)
-
-        // Upload the first image.
-        firebaseReference.child("photo.jpg").putData(photoData, metadata: metaData) { _, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            // Upload the second image.
-            firebaseReference.child("tag.jpg").putData(tagData, metadata: metaData) { _, error in
-                if let error = error {
-                    completion(.failure(error))
-                    return
-                }
-                
-                // Get download URLs for both images.
-                firebaseReference.child("photo.jpg").downloadURL { photoURL, _ in
-                    firebaseReference.child("tag.jpg").downloadURL { tagURL, _ in
-                        if let photoURL = photoURL, let tagURL = tagURL {
-                            // Return the download URLs in the completion handler.
-                            completion(.success((photoURL, tagURL)))
-                        } else {
-                            completion(.failure(FirebaseError.downloadURLError))
-                        }
-                    }
-                }
-            }
-        }
-    }
+//    func photoUploadImage(image: [UIImage], date: String, memo: String, tagText: String, completion: @escaping (Result<(URL, URL), Error>) -> Void) {
+//        guard let userUID = authManager.currentUserUID else { return }
+//        guard image.count >= 2 else {
+//            completion(.failure(FirebaseError.imageCountError))
+//            return
+//        }
+//        
+//        let photoImageData = image[0].jpegData(compressionQuality: 0.4)
+//        let tagImageData = image[1].jpegData(compressionQuality: 0.4)
+//
+//        guard let photoData = photoImageData, let tagData = tagImageData else {
+//            completion(.failure(FirebaseError.imageDataError))
+//            return
+//        }
+//        
+//        let metaData = StorageMetadata()
+//        metaData.contentType = "image/jpeg"
+//        
+//        let imageName = userUID
+//        
+//        let firebaseReference = Storage.storage().reference().child(imageName).child(UUID().uuidString)
+//
+//        // Upload the first image.
+//        firebaseReference.child("photo.jpg").putData(photoData, metadata: metaData) { _, error in
+//            if let error = error {
+//                completion(.failure(error))
+//                return
+//            }
+//            
+//            // Upload the second image.
+//            firebaseReference.child("tag.jpg").putData(tagData, metadata: metaData) { _, error in
+//                if let error = error {
+//                    completion(.failure(error))
+//                    return
+//                }
+//                
+//                // Get download URLs for both images.
+//                firebaseReference.child("photo.jpg").downloadURL { photoURL, _ in
+//                    firebaseReference.child("tag.jpg").downloadURL { tagURL, _ in
+//                        if let photoURL = photoURL, let tagURL = tagURL {
+//                            // Return the download URLs in the completion handler.
+//                            completion(.success((photoURL, tagURL)))
+//                        } else {
+//                            completion(.failure(FirebaseError.downloadURLError))
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     func uploadPhotoImage(image: [UIImage], completion: @escaping (Result<(URL, URL), Error>) -> Void) {
         guard image.count >= 2 else {
