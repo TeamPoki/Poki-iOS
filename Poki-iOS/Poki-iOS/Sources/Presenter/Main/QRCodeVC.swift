7184a335be3485abdd26f36235782d8839dfd86e
Insu <insu2405@gmail.com>
2023-11-06 18:57:26 +0900

    [Feat]: QR코드 인식 후 해당 URL으로 진입 로직 구현

--- Poki-iOS/Poki-iOS/Sources/Presenter/Main/QRCodeVC.swift
+++ Poki-iOS/Poki-iOS/Sources/Presenter/Main/QRCodeVC.swift
@@ -40,7 +40,24 @@ final class QRCodeVC: UIViewController {
             print("Failed to get the camera device")
             return
         }
-                
+        
+        do {
+            try captureDevice.lockForConfiguration()
+            
+            // 카메라가 연속 자동 초점 모드를 지원하는지 확인
+            if captureDevice.isFocusModeSupported(.continuousAutoFocus) {
+                // 연속 자동 초점 모드를 설정
+                captureDevice.focusMode = .continuousAutoFocus
+            }
+            
+            // 추가로, 자동 노출 및 화이트 밸런스 설정을 원할 경우에는 요 라인에다가 설정하면 됨.
+            
+            captureDevice.unlockForConfiguration()
+        } catch {
+            print("Unable to lock camera for configuration: \(error)")
+            return
+        }
+        
         cameraDevice = captureDevice
     }
         
@@ -84,6 +101,10 @@ final class QRCodeVC: UIViewController {
     }
         
     private func downloadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        // HTTP 헤더에 "Accept" 항목을 "image/*"로 설정
+        var request = URLRequest(url: url)
+        request.setValue("image/*", forHTTPHeaderField: "Accept")
+        
         URLSession.shared.dataTask(with: url) { data, _, error in
             if let data = data, let image = UIImage(data: data) {
                 DispatchQueue.main.async {
@@ -120,7 +141,6 @@ final class QRCodeVC: UIViewController {
             self.videoPreviewLayer?.frame = self.view.layer.bounds
             self.view.layer.addSublayer(self.videoPreviewLayer!)
         }
-        // startRunning을 실행시켜야 화면이 보이게 됩니다.
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
         }
@@ -149,26 +169,46 @@ final class QRCodeVC: UIViewController {
 extension QRCodeVC: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
-           let stringValue = metadataObject.stringValue,
-           let url = URL(string: stringValue)
-        {
-            downloadImage(from: url) { [weak self] image in
-                guard let self = self else { return }
-
-                if let validImage = image {
-                    let addPhotoVC = AddPhotoVC()
-                    addPhotoVC.addPhotoView.photoImageView.image = validImage
-                    self.navigationController?.pushViewController(addPhotoVC, animated: true)
-                } else {
-                    let alert = UIAlertController(title: "오류", message: "URL이 만료되었거나 이미지가 없습니다.", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "확인", style: .default))
+           let stringValue = metadataObject.stringValue {
+            // stringValue 시작 부분이 "http"인 경우 "https"로 변환 -> 보안 때문에 변환 해줘야함.
+            let secureStringValue = stringValue.hasPrefix("http://") ? "https" + stringValue.dropFirst(4) : stringValue
+            
+            if let url = URL(string: secureStringValue) {
+                downloadImage(from: url) { [weak self] image in
+                    guard let self = self else { return }
+                    
+                    DispatchQueue.main.async {
+                        if let validImage = image {
+                            let addPhotoVC = AddPhotoVC()
+                            addPhotoVC.addPhotoView.photoImageView.image = validImage
+                            self.navigationController?.pushViewController(addPhotoVC, animated: true)
+                        } else {
+                            // 이미지를 다운로드하지 못했을 때 사용자에게 알림 및 URL로 리디렉션 옵션 제공
+                            let alert = UIAlertController(title: "오류", message: "이미지를 다운로드할 수 없습니다.\n웹사이트로 이동하시겠습니까?", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "이동", style: .default, handler: { _ in
+                                if let url = URL(string: secureStringValue), UIApplication.shared.canOpenURL(url) {
+                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                }
+                            }))
+                            alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { _ in
+                                self.captureSession.startRunning()
+                            }))
+                            self.present(alert, animated: true)
+                        }
+                        self.tabBarController?.tabBar.isHidden = false
+                    }
+                }
+            } else {
+                // 유요한 URL이 아닐경우, 사용자에게 알림 띄우기
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "오류", message: "유효한 URL이 아닙니다.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+                        self.captureSession.startRunning()
+                    }))
                     self.present(alert, animated: true)
                 }
-                
-                // QR 코드 스캔이 완료되었으면 탭바 다시 표시
-                self.tabBarController?.tabBar.isHidden = false
             }
         }
     }
