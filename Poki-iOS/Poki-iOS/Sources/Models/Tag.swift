3f34b2fcb8e4a6621469f364697e471bb2924f64
Hongsik Choi <playhong16@gmail.com>
2023-11-15 15:21:47 +0900

    #321 [Fix]: 플레이인더박스 태그 이미지 URL 변경으로 인한 코드 수정

--- Poki-iOS/Poki-iOS/Sources/Models/Tag.swift
+++ Poki-iOS/Poki-iOS/Sources/Models/Tag.swift
@@ -28,7 +28,7 @@ struct TagData {
         TagModel(tagLabel: "포토드링크", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Brand%2F%E1%84%91%E1%85%A9%E1%84%90%E1%85%A9%E1%84%83%E1%85%B3%E1%84%85%E1%85%B5%E1%86%BC%E1%84%8F%E1%85%B3.png?alt=media&token=954eda82-d4fe-4be4-8cbd-9a81d220f4b7"),
         TagModel(tagLabel: "포토이즘", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Brand%2F%E1%84%91%E1%85%A9%E1%84%90%E1%85%A9%E1%84%8B%E1%85%B5%E1%84%8C%E1%85%B3%E1%86%B7.png?alt=media&token=68f808a2-d528-4624-83e5-ca22c3757220"),
         TagModel(tagLabel: "모노맨션", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Brand%2F%E1%84%86%E1%85%A9%E1%84%82%E1%85%A9%E1%84%86%E1%85%A6%E1%86%AB%E1%84%89%E1%85%A7%E1%86%AB.png?alt=media&token=6e8c31a2-f1d6-4f7c-8828-76f2798f1c95"),
-        TagModel(tagLabel: "플레이인더박스", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Brand%2FFrame%2061148.png?alt=media&token=fa0b9582-98b7-4f29-bf35-aa054fc1b7b7"),
+        TagModel(tagLabel: "플레이인더박스", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Brand%2F%E1%84%91%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%83%E1%85%A5%E1%84%87%E1%85%A1%E1%86%A8%E1%84%89%E1%85%B3.png?alt=media&token=25acd777-0bd4-4138-8898-b6320985a065"),
         TagModel(tagLabel: "비분류 브랜드", tagImage: "https://firebasestorage.googleapis.com/v0/b/poki-ios-87d7e.appspot.com/o/Group%2061116.png?alt=media&token=66f5fb03-cd1a-4d74-aa0d-1f781cdd9879&_gl=1*3i3q4a*_ga*MzI1NjE1OTMzLjE2OTg5ODk0NDY.*_ga_CW55HF8NVT*MTY5ODk4OTQ0Ni4xLjEuMTY5ODk5MDQ3My40Ny4wLjA.")
     ]
 }
